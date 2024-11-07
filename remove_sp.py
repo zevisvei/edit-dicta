49a758fbc09b149a41ded1dbc9c225a05e22d0a4
zevisvei <101482799+zevisvei@users.noreply.github.com>
2024-11-07 17:35:15 +0000

    rename

--- remove_sp.py
+++ remove_sp.py
@@ -4,13 +4,13 @@ def remove_apostrophe_from_names(base_dir):
     for root, dirs, files in os.walk(base_dir, topdown=False):
         # Rename files
         for filename in files:
-            new_filename = filename.replace("'", "")
+            new_filename = filename.replace("''", "")
             if new_filename != filename:
                 os.rename(os.path.join(root, filename), os.path.join(root, new_filename))
         
         # Rename directories
         for dirname in dirs:
-            new_dirname = dirname.replace("'", "")
+            new_dirname = dirname.replace("''", "")
             if new_dirname != dirname:
                 os.rename(os.path.join(root, dirname), os.path.join(root, new_dirname))
 
