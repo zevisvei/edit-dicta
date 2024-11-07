2a1e294e37a23a77a60a48428632751ce4c6286c
zevisvei <101482799+zevisvei@users.noreply.github.com>
2024-11-07 19:21:22 +0200

    Update remove_sp.py

--- remove_sp.py
+++ remove_sp.py
@@ -2,18 +2,18 @@ import os
 
 def remove_apostrophe_from_names(base_dir):
     for root, dirs, files in os.walk(base_dir, topdown=False):
-            # Rename files
-                    for filename in files:
-                                new_filename = filename.replace("'", "")
-                                            if new_filename != filename:
-                                                            os.rename(os.path.join(root, filename), os.path.join(root, new_filename))
-                                                                    
-                                                                            # Rename directories
-                                                                                    for dirname in dirs:
-                                                                                                new_dirname = dirname.replace("'", "")
-                                                                                                            if new_dirname != dirname:
-                                                                                                                            os.rename(os.path.join(root, dirname), os.path.join(root, new_dirname))
+        # Rename files
+        for filename in files:
+            new_filename = filename.replace("'", "")
+            if new_filename != filename:
+                os.rename(os.path.join(root, filename), os.path.join(root, new_filename))
+        
+        # Rename directories
+        for dirname in dirs:
+            new_dirname = dirname.replace("'", "")
+            if new_dirname != dirname:
+                os.rename(os.path.join(root, dirname), os.path.join(root, new_dirname))
 
-                                                                                                                            # Example usage
-                                                                                                                            base_directory = "/path/to/your/directory"
-                                                                                                                            remove_apostrophe_from_names(base_directory)
+# Example usage
+base_directory = "ספרים"
+remove_apostrophe_from_names(base_directory)
